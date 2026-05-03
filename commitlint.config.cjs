86c623e001f8adc7be56e427ab6e361d78f0fa08
Ashraf Qopiah <ashrafmo-1@outlook.com>
2026-05-03 06:17:00 +0300

    feat: init husky pre commit

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,13 @@
-export default {
+module.exports = {
     extends: ['@commitlint/config-conventional'],
+    formatter: '@commitlint/format',
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']
+      ],
+      'subject-empty': [2, 'never'],
+      'type-empty': [2, 'never'],
+    },
   };
